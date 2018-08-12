643671654913a560b2c9aee6526ab23d33825d37
Mujtaba Gul <mujtabagul@Mujtabas-MacBook-Air.local>
2018-08-12 17:15:32 -0500

    Second commit

--- Data_Cleaning_Mujtaba.R
+++ Data_Cleaning_Mujtaba.R
@@ -1,5 +1,19 @@
+library(ggplot2)
+library(dplyr)
+
+
 setwd("/Users/mujtabagul/House_Prices/")
-
-
 dat = fread("train.csv")
 
+### FEATURE ENGINEERING
+dat$Remodelling = ifelse((dat$YearRemodAdd - dat$YearBuilt) > 0, "YES", "NO")
+dat$BsmtUnf_prop = dat$BsmtUnfSF/dat$TotalBsmtSF %>% is.finite()
+dat$age_house = dat$YrSold - dat$YearBuilt
+
+### PLOTS
+    dat %>% group_by(age_house) %>%
+             summarize(SalePrice = mean(SalePrice)) %>%
+             ggplot(aes(age_house,SalePrice)) +
+             geom_point() +
+             geom_smooth() +
+             ggtitle("Age of House vs Sale Price")
