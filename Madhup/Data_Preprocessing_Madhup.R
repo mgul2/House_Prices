2f39b7f297261e29f55564537312757c3d85942e
Madhup Garg <mgarg20@uic.edu>
2018-08-17 15:59:07 -0500

    Modify Data Processing script

--- Madhup/Data_Preprocessing_Madhup.R
+++ Madhup/Data_Preprocessing_Madhup.R
@@ -1,6 +1,7 @@
 getwd()
 library(dplyr)
 attach(train)
+library(forcats) 
 setwd("/Users/madhupgarg123/House_Prices/Datasets")
 
 dim(train); 
@@ -25,18 +26,22 @@ to_factor = c(2, 3, 18, 19, 20, 21, 48:53, 55, 57, 62, 72, 77, 78, 82)
 
 train[to_factor] = lapply(train[to_factor], factor)
 
-# Check null values for each column
-
-for (i in colnames(train)){
-  print(i)
-  print(sum(is.na(train[i])))
-}
-
 # Divide dataset into 2 halves, Madhup will process columns 44-86
 
 train_mujtaba = train[, c(1:43)]
 train_madhup = train[, c(44:85)]
 dim(train_madhup)
 dim(train_mujtaba)
+
+# Handle Missing Values
+
+# Create a dataframe which shows all columns with number of missing values:
+missing_count <-sapply(train_madhup, function(y) sum(length(which(is.na(y)))))
+missing_count <- data.frame(missing_count)
+View(missing_count)
+
+#Process FireplaceQu, NA means No Fireplace
+
+train_madhup$FireplaceQu = fct_explicit_na(train_madhup$FireplaceQu, na_level = "No Fireplace")
 
 View(train_madhup)
